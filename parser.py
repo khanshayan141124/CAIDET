67c8168adcd3992e47c1d0400248729dc929dcdd
khanshayan141124 <khanshayan3272@gmail.com>
2026-01-02 19:12:05 +0530

    Implement listen function for voice input simulation
    Added a listen function to simulate voice input.

--- parser.py
+++ parser.py
@@ -1,2 +1,7 @@
-def parse(command):
-    return command
+def listen():
+    """
+    Simulates voice input.
+    Later this will be replaced by real speech-to-text.
+    """
+    command = input("🎙️ You: ")
+    return command.lower()
